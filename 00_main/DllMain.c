569a67d6e4e8e678ad3de24f74c14818038f2bae
5𝓸𝓾𝑓𝑓!𝓮́💚 <akiho_rurikawa@outlook.jp>
2026-01-09 10:19:06 +0900

    Add DllMain function for DLL entry point

---
+++ 00_main/DllMain.c
@@ -0,0 +1,20 @@
+// cl.exe /nologo /utf-8 /LD /Zi /Od /Ob0 /RTC1 /GS /sdl /W4 /MDd <source.c> /link /DEBUG:FULL /INCREMENTAL
+// cl.exe /nologo /utf-8 /LD /O2 /GL /GS /sdl /guard:cf /Qspectre /Gy /Gw /GF /W4 /MD <source.c> /link /LTCG /INCREMENTAL:NO /OPT:REF /OPT:ICF /DYNAMICBASE /NXCOMPAT /HIGHENTROPYVA /CETCOMPAT /DEBUG:NONE
+
+#include <Windows.h>
+
+BOOL APIENTRY DllMain ( HMODULE hModule,
+                        DWORD dwReason,
+                        LPVOID lpReserved)
+{
+    switch (dwReason)
+    {
+        case DLL_PROCESS_ATTACH:
+        case DLL_THREAD_ATTACH:
+        case DLL_THREAD_DETACH:
+        case DLL_PROCESS_DETACH:
+            break;
+    }
+
+    return TRUE;
+}
