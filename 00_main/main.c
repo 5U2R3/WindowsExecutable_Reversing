17fbe66781b46a09b4c9f083558a0110bd137638
5𝓸𝓾𝑓𝑓!𝓮́💚 <akiho_rurikawa@outlook.jp>
2026-01-09 10:17:34 +0900

    Initialize main.c with a basic main function

---
+++ 00_main/main.c
@@ -0,0 +1,7 @@
+// cl.exe /nologo /utf-8 /Zi /Od /Ob0 /RTC1 /GS /sdl /W3 /MDd <source.c> /link /INCREMENTAL /DEBUG:FULL
+// cl.exe /nologo /utf-8 /O2 /GL /Ob2 /GS /guard:cf /Qspectre /sdl /Gy /Gw /GF /W3 /MD <source.c> /link /LTCG /INCREMENTAL:NO /OPT:REF /OPT:ICF /DEBUG:NONE
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
